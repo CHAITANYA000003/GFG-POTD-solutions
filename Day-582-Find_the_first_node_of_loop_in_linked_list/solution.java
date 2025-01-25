1874c6375f248c51b929e729f715c7e1299a3987
CHAITANYA000003 <chaitanya2003217@gmail.com>
2025-01-25 09:55:29 +0530

    Add Day-582

---
+++ Day-582-Find_the_first_node_of_loop_in_linked_list/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public static Node findFirstNode(Node head) {
+        // code here
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                break;
+            }
+        }
+        if (slow != fast) {
+            return new Node(-1);
+        }
+
+        slow = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
